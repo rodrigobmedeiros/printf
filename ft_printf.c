59e9dc022c2b12d72d1be5f12e5a75bfc1c8bedf
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2023-11-17 15:46:39 -0300

    Add treatment to strings

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,34 @@
 /*   By: robernar <robernar@student.42.rj>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 16:49:54 by robernar          #+#    #+#             */
-/*   Updated: 2023/11/17 12:51:20 by robernar         ###   ########.fr       */
+/*   Updated: 2023/11/17 15:37:58 by robernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include "libft/libft.h"
 #include "ft_printf.h"
+
+void	ft_treat_char(va_list args, int *print_counter)
+{
+	ft_putchar_fd(va_arg(args, int), 1);
+	*print_counter = *print_counter + 1;
+}
+
+void	ft_treat_str(va_list args, int *print_counter)
+{
+	char	*str;
+
+	str = va_arg(args, char *);
+	ft_putstr_fd(str, 1);
+	*print_counter = *print_counter + ft_strlen(str);
+}
+
+PrintOption ft_get_treatment(char c)
+{
+	if (c == 'c')
+		return (ft_treat_char);
+	if (c == 's')
+		return (ft_treat_str);
+	return (NULL);
+}
 
 void	ft_define_treatment(char *str, va_list args, int *print_counter)
 {
@@ -19,13 +42,7 @@ void	ft_define_treatment(char *str, va_list args, int *print_counter)
 	
 	special_chars = "cspdiuxX%";
 	special_char = ft_memchr(special_chars, *str, ft_strlen(special_chars));
-	if (*special_char == 'c')
-	{
-		// tratamento para no do caso do digito ser c 
-		// dai preciso de um tratamento para caracter
-		ft_putchar_fd(va_arg(args, int), 1);
-		*print_counter = *print_counter  + 1;
-	}
+	ft_get_treatment(*special_char)(args, print_counter);
 }
 
 int	ft_printf(const char *format,  ...)
@@ -40,7 +57,7 @@ int	ft_printf(const char *format,  ...)
 	while (*str)
 		if (*str == '%')
 		{
-			str++; // proximo caracter apos o %
+			str++;
 			ft_define_treatment(str++, args, &print_counter);
 		}
 		else
@@ -55,6 +72,15 @@ int	ft_printf(const char *format,  ...)
 #include <stdio.h>
 int	main()
 {
-	printf("number of printed chars - system: %d\n", printf("rodrigo%c\n", 'A'));
-	printf("number of printed chars - custom: %d\n", ft_printf("rodrigo%c\n", 'A'));
+	// Teste para multiplos caracteres
+	printf("number of printed chars - system: %d\n", printf("rodrigo%c - %c\n", 'A', 'B'));
+	printf("number of printed chars - custom: %d\n", ft_printf("rodrigo%c - %c\n", 'A', 'B'));
+
+	// Teste para uma string
+	printf("number of printed chars - system: %d\n", printf("rodrigo - %s\n", "RODRIGO"));
+	printf("number of printed chars - custom: %d\n", ft_printf("rodrigo - %s\n", "RODRIGO"));
+
+ 	// Teste para multiplas string
+ 	printf("number of printed chars - system: %d\n", printf("rodrigo - %s %s\n", "RODRIGO", "BERNARDO"));
+ 	printf("number of printed chars - custom: %d\n", ft_printf("rodrigo - %s %s\n", "RODRIGO", "BERNARDO"));
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,11 +6,13 @@
 /*   By: robernar <robernar@student.42.rj>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 18:23:32 by robernar          #+#    #+#             */
-/*   Updated: 2023/11/16 18:24:45 by robernar         ###   ########.fr       */
+/*   Updated: 2023/11/17 15:26:53 by robernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 # include <stdarg.h>
+# include "libft/libft.h"
+typedef void (*PrintOption)(va_list, int *);
 #endif
 
