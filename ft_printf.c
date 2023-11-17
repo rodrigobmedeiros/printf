4443ba35dd7d87e55e2cc219a58c50af1c45a226
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2023-11-17 12:09:29 -0300

    Trying to fix the problem =D

--- ft_printf.c
+++ ft_printf.c
@@ -6,33 +6,47 @@
 /*   By: robernar <robernar@student.42.rj>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 16:49:54 by robernar          #+#    #+#             */
-/*   Updated: 2023/11/16 18:38:48 by robernar         ###   ########.fr       */
+/*   Updated: 2023/11/17 11:27:22 by robernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft/libft.h"
 #include "ft_printf.h"
 
+void	ft_define_treatment(char *str, va_list args, int *print_counter)
+{
+	char	*special_chars;
+	char	*special_char;
+
+	special_chars = "cspdiuxX%";
+	special_char = ft_memchr(special_chars, *str);
+	if (*special_char == 'c')
+		// tratamento para no do caso do digito ser c 
+		// dai preciso de um tratamento para caracter
+		ft_putchar_fd(va_arg(args, char), 1);
+		*print_couter = *print_counter++;
+}
+
 int	ft_printf(const char *format,  ...)
 {
 	va_list	args;
+	int	print_counter;
 	char	*special_chars;
 	char	*str;
-	int	len;
 
 	va_start(args, format);
 	str = (char *)format;
-	len = ft_strlen(str);
 	special_chars = "cspdiuxX%"; // caracteres que tem algum tipo de tratamento especial.
-	while (*str || len == 0)
+	while (*str)
 		if (*str == '%')
-			str++;
+			str++; // proximo caracter apos o %
+				
 		else
 			ft_putchar_fd(*str++, 1);
 			
 			// preciso verificar se existe um dos caracteres que preciso tratar.
 
 	va_end(args);
-	return (1);
+	return (print_counter);
 }
 
 #include <stdio.h>
