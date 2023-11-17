51af72b1c5ba669e04cb841e3706be9b66a96b69
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2023-11-17 12:51:47 -0300

    Add treatment to characters

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: robernar <robernar@student.42.rj>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 16:49:54 by robernar          #+#    #+#             */
-/*   Updated: 2023/11/17 11:27:22 by robernar         ###   ########.fr       */
+/*   Updated: 2023/11/17 12:51:20 by robernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft/libft.h"
@@ -16,35 +16,38 @@ void	ft_define_treatment(char *str, va_list args, int *print_counter)
 {
 	char	*special_chars;
 	char	*special_char;
-
+	
 	special_chars = "cspdiuxX%";
-	special_char = ft_memchr(special_chars, *str);
+	special_char = ft_memchr(special_chars, *str, ft_strlen(special_chars));
 	if (*special_char == 'c')
+	{
 		// tratamento para no do caso do digito ser c 
 		// dai preciso de um tratamento para caracter
-		ft_putchar_fd(va_arg(args, char), 1);
-		*print_couter = *print_counter++;
+		ft_putchar_fd(va_arg(args, int), 1);
+		*print_counter = *print_counter  + 1;
+	}
 }
 
 int	ft_printf(const char *format,  ...)
 {
 	va_list	args;
 	int	print_counter;
-	char	*special_chars;
 	char	*str;
 
 	va_start(args, format);
 	str = (char *)format;
-	special_chars = "cspdiuxX%"; // caracteres que tem algum tipo de tratamento especial.
+	print_counter = 0;
 	while (*str)
 		if (*str == '%')
+		{
 			str++; // proximo caracter apos o %
-				
+			ft_define_treatment(str++, args, &print_counter);
+		}
 		else
+		{
 			ft_putchar_fd(*str++, 1);
-			
-			// preciso verificar se existe um dos caracteres que preciso tratar.
-
+			print_counter++;
+		}
 	va_end(args);
 	return (print_counter);
 }
@@ -52,7 +55,6 @@ int	ft_printf(const char *format,  ...)
 #include <stdio.h>
 int	main()
 {
-	printf("%d\n", ft_printf("rodrigo", 1, 2, 3));
-	printf("%%");
-	printf("%d - %d", 1, 2, 3, 4, 5);
+	printf("number of printed chars - system: %d\n", printf("rodrigo%c\n", 'A'));
+	printf("number of printed chars - custom: %d\n", ft_printf("rodrigo%c\n", 'A'));
 }
