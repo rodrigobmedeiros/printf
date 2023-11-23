d9b4a9b87ffcede6498fd962a4976bbbf18d2336
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2023-11-22 22:21:20 -0300

    change typedef to pass into norminette analysis

--- ft_printf.h
+++ ft_printf.h
@@ -6,14 +6,15 @@
 /*   By: robernar <robernar@student.42.rj>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 18:23:32 by robernar          #+#    #+#             */
-/*   Updated: 2023/11/22 10:54:06 by robernar         ###   ########.fr       */
+/*   Updated: 2023/11/22 16:56:33 by robernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 # include <stdarg.h>
 # include "libft/libft.h"
-typedef void	(*PrintOption)(va_list, int *);
+
+typedef void	(*t_PrintOption)(va_list, int *);
 char	*ft_itoa_base(long int n, int base, char *digits);
-int	ft_printf(const char *format,  ...);
+int		ft_printf(const char *format, ...);
 #endif
